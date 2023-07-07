c64b9a2a6d0c61b35c1654e76d184bcd2cbfd6fd
unknown <katrin.dudina@mail.com>
2023-07-07 16:39:35 +0300

    backend

---
+++ public/core/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createSocket = exports.createRoutes = void 0;
+var routes_1 = require("./routes");
+Object.defineProperty(exports, "createRoutes", { enumerable: true, get: function () { return __importDefault(routes_1).default; } });
+var socket_1 = require("./socket");
+Object.defineProperty(exports, "createSocket", { enumerable: true, get: function () { return __importDefault(socket_1).default; } });
+//# sourceMappingURL=index.js.map
---
+++ public/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const dotenv_1 = __importDefault(require("dotenv"));
+const routes_1 = __importDefault(require("./core/routes"));
+const core_1 = require("./core");
+const app = (0, express_1.default)();
+const http = (0, http_1.createServer)(app);
+const io = (0, core_1.createSocket)(http);
+dotenv_1.default.config();
+(0, routes_1.default)(app, io);
+// @ts-ignore
+mongoose_1.default.connect(process.env.MONGO_URI);
+http.listen(process.env.PORT || 9999, function () {
+    console.log(`Server: http://localhost: ${process.env.PORT}`);
+});
+//# sourceMappingURL=index.js.map
